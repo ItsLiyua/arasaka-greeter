1dbe8d00acbba209f7a52c8fd3f6e3303629a3e5
Liyua <liyua@duck.com>
2025-07-21 14:21:49 +0200

    Added flake utils

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
 
     ags = {
       url = "github:aylur/ags";
@@ -14,59 +15,60 @@
     {
       self,
       nixpkgs,
+      flake-utils,
       ags,
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      pname = "my-shell";
-      version = "1.0";
-      entry = "app.ts";
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        pname = "my-shell";
+        version = "1.0";
+        entry = "app.ts";
 
-      astalPackages = with ags.packages.${system}; [
-        io
-        astal4 # or astal3 for gtk3
-        # notifd tray wireplumber
-      ];
-
-      extraPackages = astalPackages ++ [
-        pkgs.libadwaita
-        pkgs.libsoup_3
-      ];
-    in
-    {
-      packages.${system}.default = pkgs.stdenv.mkDerivation {
-        inherit pname version;
-        src = ./.;
-
-        nativeBuildInputs = with pkgs; [
-          wrapGAppsHook
-          gobject-introspection
-          ags.packages.${system}.default
+        astalPackages = with ags.packages.${system}; [
+          io
+          astal4 # or astal3 for gtk3
+          # notifd tray wireplumber
         ];
 
-        buildInputs = extraPackages ++ [ pkgs.gjs ];
+        extraPackages = astalPackages ++ [
+          pkgs.libadwaita
+          pkgs.libsoup_3
+        ];
+      in
+      {
+        packages.default = pkgs.stdenv.mkDerivation {
+          inherit pname version;
+          src = ./.;
 
-        installPhase = ''
-          runHook preInstall
+          nativeBuildInputs = with pkgs; [
+            wrapGAppsHook
+            gobject-introspection
+            ags.packages.${system}.default
+          ];
 
-          mkdir -p $out/bin
-          mkdir -p $out/share
-          cp -r * $out/share
-          ags bundle ${entry} $out/bin/${pname} -d "SRC='$out/share'"
+          buildInputs = extraPackages ++ [ pkgs.gjs ];
 
-          runHook postInstall
-        '';
-      };
+          installPhase = ''
+            runHook preInstall
 
-      devShells.${system} = {
-        default = pkgs.mkShell {
+            mkdir -p $out/bin
+            mkdir -p $out/share
+            cp -r * $out/share
+            ags bundle ${entry} $out/bin/${pname} -d "SRC='$out/share'"
+
+            runHook postInstall
+          '';
+        };
+
+        devShells.default = pkgs.mkShell {
           buildInputs = [
             (ags.packages.${system}.default.override {
               inherit extraPackages;
             })
           ];
         };
-      };
-    };
+      }
+    );
 }
