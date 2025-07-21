1f864d5d2a493418731dad58bef71fdc5cb2cd60
Liyua <liyua@duck.com>
2025-07-21 23:59:59 +0200

    AGS now launches inside weston

--- nix/package.nix
+++ nix/package.nix
@@ -7,6 +7,8 @@
   gjs,
   libadwaita,
   libsoup_3,
+  bash,
+  weston,
 }:
 stdenv.mkDerivation rec {
   pname = "arasaka-greeter";
@@ -31,9 +33,15 @@ stdenv.mkDerivation rec {
     runHook preInstall
 
     mkdir -p $out/bin
-    mkdir -p $out/share
-    cp -r $src/* $out/share
-    ags bundle $src/app.ts $out/bin/${pname} -d "SRC='$out/share'"
+    mkdir -p $out/share/arasaka-greeter/ags
+    cp -r $src/app.ts $src/style.scss $src/widget $out/share/arasaka-greeter/ags
+    ags bundle $src/app.ts $out/bin/${pname} -d "SRC='$out/share/arasaka-greeter/ags'"
+
+    cp $src/resources/weston.ini $out/share/arasaka-greeter/weston.ini
+    substituteInPlace $out/share/arasaka-greeter/weston.ini --replace "{GREETER}" "$out/bin/arasaka-greeter"
+
+    cp $src/resources/launch-arasaka-greeter $out/bin/launch-arasaka-greeter
+    substituteInPlace $out/bin/launch-arasaka-greeter --replace "{BASH}" "${bash}" --replace "{WESTON}" "${weston}" --replace "{WESTON_CONFIG}" "$out/share/arasaka-greeter/weston.ini"
 
     runHook postInstall
   '';
