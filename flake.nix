86b2691850fbbde6572262f8d21f19c4ab6b0f3a
Liyua <liyua@duck.com>
2025-07-21 14:15:24 +0200

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,72 @@
+{
+  description = "A GreetD frontend built with AGS and themed in the style of the arasaka corporation from Cyberpunk 2077";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      ags,
+    }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      pname = "my-shell";
+      version = "1.0";
+      entry = "app.ts";
+
+      astalPackages = with ags.packages.${system}; [
+        io
+        astal4 # or astal3 for gtk3
+        # notifd tray wireplumber
+      ];
+
+      extraPackages = astalPackages ++ [
+        pkgs.libadwaita
+        pkgs.libsoup_3
+      ];
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        inherit pname version;
+        src = ./.;
+
+        nativeBuildInputs = with pkgs; [
+          wrapGAppsHook
+          gobject-introspection
+          ags.packages.${system}.default
+        ];
+
+        buildInputs = extraPackages ++ [ pkgs.gjs ];
+
+        installPhase = ''
+          runHook preInstall
+
+          mkdir -p $out/bin
+          mkdir -p $out/share
+          cp -r * $out/share
+          ags bundle ${entry} $out/bin/${pname} -d "SRC='$out/share'"
+
+          runHook postInstall
+        '';
+      };
+
+      devShells.${system} = {
+        default = pkgs.mkShell {
+          buildInputs = [
+            (ags.packages.${system}.default.override {
+              inherit extraPackages;
+            })
+          ];
+        };
+      };
+    };
+}
