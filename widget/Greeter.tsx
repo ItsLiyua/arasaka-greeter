8bf35fa2000ca809eb54e42b2a93c6d4c1ef6efc
Liyua <liyua@duck.com>
2025-07-23 01:04:11 +0200

    Got a window

---
+++ widget/Greeter.tsx
@@ -0,0 +1,14 @@
+import app from "ags/gtk4/app";
+import Gtk from "gi://Gtk?version=4.0";
+
+export default function Greeter() {
+  const win = new Gtk.Window({
+    visible: true,
+    name: "greeter",
+    cssClasses: ["greeter"],
+    application: app,
+    child: new Gtk.Label({ label: "Hello World", visible: true }),
+  });
+  win.show();
+  return win;
+}
